df7f5d9650992253f75e1b72441ba67e0087236d
Sudipto Chandra <dipu.sudipta@gmail.com>
2025-05-06 02:47:31 +0400

    Save gold price as artifact

--- jobs/gold_price_slack_alert.py
+++ jobs/gold_price_slack_alert.py
@@ -1,3 +1,5 @@
+from dataclasses import asdict
+import json
 import logging
 import os
 import sys
@@ -11,9 +13,22 @@ sys.path.insert(0, ROOT_DIR)
 def main():
     from marketbot.context import ServerContext
     ctx = ServerContext()
+
     prices = ctx.gold.get_latest_gold_prices()
+    artifact_data = json.dumps(
+        [asdict(result) for result in prices],
+        indent=4,
+    )
+    print(artifact_data)
+
+    with open('gold-price.json', 'w', encoding='utf-8') as f:
+        f.write(artifact_data)
+
     message = ctx.gold.build_slack_message(prices)
-    ctx.gold.send_slack_alert(message)
+    print(message['text'])
+
+    result = ctx.gold.send_slack_alert(message)
+    print(result)
 
 
 if __name__ == '__main__':
